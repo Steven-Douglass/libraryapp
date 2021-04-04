f5565c8700fcefccbca5905339671aca94790b9e
Steve940 <smd940@gmail.com>
2021-04-04 08:21:49 -0400

    Creating urls and views for viewing Model information

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -3,5 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /library/
     path('', views.index, name='index'),
+    # ex: /library/author/2
+    path('author/<int:author_id>/', views.author_detail, name='author_detail'),
+    # ex: /library/book/2
+    path('book/<int:book_id>/', views.book_detail, name='book_detail'),
+    # ex: /library/location/1
+    path('location/<int:library_id>/', views.library_detail, name='library_detail'),
+    # ex: /library/member/1
+    path('member/<int:member_id>/', views.member_detail, name='member_detail'),
 ]
--- libraryapp/views.py
+++ libraryapp/views.py
@@ -2,3 +2,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the library index.")
+
+def author_detail(request, author_id):
+    return HttpResponse("You're looking at author %s." % author_id)
+
+def book_detail(request, book_id):
+    return HttpResponse("You're looking at book %s." % book_id)
+
+def library_detail(request, library_id):
+    return HttpResponse("You're looking at library %s." % library_id)
+
+def member_detail(request, member_id):
+    return HttpResponse("You're looking at member %s." % member_id)
