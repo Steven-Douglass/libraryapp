e45fb5053a6bfc0909d11a8bd414aac683abe6e3
Steve940 <smd940@gmail.com>
2021-04-06 12:21:50 -0400

    Added custom HTML templates for detail view pages Adjust views.py to render new HTML templates

--- libraryapp/views.py
+++ libraryapp/views.py
@@ -1,16 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render
+
+from .models import Book, Author, Member, Library
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the library index.")
+    book_list = Book.objects.order_by('id')
+    context = { 'book_list': book_list }
+    return render(request, 'libraryapp/index.html', context)
 
 def author_detail(request, author_id):
-    return HttpResponse("You're looking at author %s." % author_id)
+    author = get_object_or_404(Author, pk=author_id)
+    return render(request, 'libraryapp/authordetail.html', {'author': author})
 
 def book_detail(request, book_id):
-    return HttpResponse("You're looking at book %s." % book_id)
+    book = get_object_or_404(Book, pk=book_id)
+    return render(request, 'libraryapp/bookdetail.html', {'book': book})
 
 def library_detail(request, library_id):
-    return HttpResponse("You're looking at library %s." % library_id)
-
+    library = get_object_or_404(Library, pk=library_id)
+    return render(request, 'libraryapp/librarydetail.html', {'library': library})
+    
 def member_detail(request, member_id):
-    return HttpResponse("You're looking at member %s." % member_id)
+    member = get_object_or_404(Member, pk=member_id)
+    return render(request, 'libraryapp/memberdetail.html', {'member': member})
